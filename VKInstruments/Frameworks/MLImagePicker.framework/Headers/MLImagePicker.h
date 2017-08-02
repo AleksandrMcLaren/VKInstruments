5f267e7e189bcfba0f6d90b4aa4e82fa5861d297
amakarov <amakarov@enaza.ru>
2017-08-02 11:45:25 +0500

    - fix

---
+++ VKInstruments/Frameworks/MLImagePicker.framework/Headers/MLImagePicker.h
@@ -0,0 +1,17 @@
+//
+//  MLImagePicker.h
+//  MLImagePicker
+//
+//  Created by Aleksandr on 27/07/2017.
+//  Copyright © 2017 Aleksandr Makarov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MLImagePicker.
+FOUNDATION_EXPORT double MLImagePickerVersionNumber;
+
+//! Project version string for MLImagePicker.
+FOUNDATION_EXPORT const unsigned char MLImagePickerVersionString[];
+
+
